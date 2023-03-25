5af19c397757f1078820ef20a1c74b9a05af6b68
Razvan <termita@gmail.com>
2023-03-25 21:48:27 +0800

    meh

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { render } from 'preact'
 import './App.css'
 import { App as CapacitorApp } from '@capacitor/app';
 import Main from './pages/Main/Main';
+import Router from 'preact-router';
 
 // CapacitorApp.addListener('appStateChange', ({ isActive }) => {
 //   alert(`App state changed. Is active? ${isActive}`);
@@ -26,5 +27,12 @@ CapacitorApp.addListener('backButton', function() {
     CapacitorApp.exitApp()
 })
 
-render(<Main ></Main>, document.getElementById('app') as HTMLElement)
+render(<Main></Main>, document.getElementById('app') as HTMLElement)
+
+// render(      (<Router>
+//   <div path="/">Home</div>
+//   <div path="/projects">Projects</div>
+//   <div path="/contact">Contact</div>
+//   <div path="/fuel">Fuel</div>
+// </Router>), document.getElementById('app') as HTMLElement)
 
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -35,9 +35,8 @@ export default function Main(props: unknown) {
         return <ToolLauncher {...launcher} />
       })}
 
-      <Match path="/">{function () {
-        console.log(arguments)
-        return <pre>{'yay'}</pre>
+      <Match path="/">{function ({url}: {url: string}) {
+        return <pre>{url}</pre>
       }}
       </Match>
 
